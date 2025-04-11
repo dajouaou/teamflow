1ed6084576afb8ded55fea739e9a575b176e064d
Laibakhan123 <24054992@student.hhs.nl>
2025-04-11 16:14:20 +0200

    login

---
+++ src/main/java/com/example/chatapplicatie/LoginController.java
@@ -0,0 +1,31 @@
+package com.example.chatapplicatie;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class LoginController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/login.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Login - TeamFlow");
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
